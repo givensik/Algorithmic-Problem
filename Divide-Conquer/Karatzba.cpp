fc187c22666382422877b41805d11aa307fff774
givensik <kjs4072@icloud.com>
2024-02-14 22:30:07 +0900

    2024/2/14 큰수 곱셈

---
+++ Divide-Conquer/Karatzba.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> multiply(const vector<int>& a, const vector<int>& b);
+void normalize(vector<int>& num);
+
+int main(){
+	vector<int> A,B;
+	string s,t; 
+	getline(cin, s);
+	
+	getline(cin, t);
+	for(int i = s.size()-1; i >= 0 ; i--){
+		A.push_back(s[i] - '0');
+	}
+	
+	for(int i = t.size()-1; i >= 0; i--){
+		B.push_back(t[i] - '0');
+	}
+
+	for(vector<int>::iterator itr = A.begin(); itr != A.end(); ++itr){
+		cout << *itr;
+	}
+	cout << "\n";
+	
+	for(vector<int>::iterator itr = B.begin(); itr != B.end(); ++itr){
+		cout << *itr;
+	}
+	cout << "\n";
+
+	vector<int> C = multiply(A,B);
+
+	for(vector<int>::iterator itr = C.end(); itr != C.begin(); --itr){
+		cout << *itr;
+	}
+	
+
+	return 0;
+}
+
+
+
+
+vector<int> multiply(const vector<int>& a, const vector<int>& b){
+	vector<int> c(a.size() + b.size() + 1, 0); // a,b 사이즈 + 1 만큼 0을 채우기
+
+	for(int i=0; i < a.size(); i++){
+		for(int j=0; j< b.size(); j++){
+			c[i+j] += a[i] * b[j];
+			printf("c[%d] = %d\n",i+j, c[i+j]);
+		}
+	}
+
+	normalize(c);
+	return c;
+}
+
+void normalize(vector<int>& num){// num이 넘어가서 바뀌어야하니까 레퍼런스로 넘기기
+	num.push_back(0);
+
+	for(int i = 0; i<num.size(); i++){
+		if(num[i] >= 10){
+			num[i+1] += num[i]/10;
+			num[i] = num[i]%10;
+		}
+	}
+
+
+}
+
+
+/*
+				5 6 7 8
+				1 2 3 4
+				20  24  28  32
+			15	18	21	24
+		10	12	14	16	
+	5	6	7	8
+
+
+
+
+*/
